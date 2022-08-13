a5d0271c35310baef203b326d4c8148af9f1d7cd
Chavez valentin <chavezvalentin414@gmail.com>
2022-08-12 22:16:37 -0300

    job loop

---
+++ ejercicio3/src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.Array;
+
+public class Main {
+    public static void main(String[] args) {
+    String [] nombres = new String[]{"jorge","pedro","mati"};
+    for (int i = 0; i < nombres.length;i++){
+        System.out.println("Hola bienvenido " + nombres[i]);
+    }
+    }
+
+}
