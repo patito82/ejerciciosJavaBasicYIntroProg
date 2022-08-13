73e7410deeed7d96e261901357bc7e56bafcca91
Chavez valentin <chavezvalentin414@gmail.com>
2022-08-12 22:57:23 -0300

    trabajo resubido

--- ejercicio3/src/Main.java
+++ ejercicio3/src/Main.java
@@ -3,8 +3,11 @@ import java.sql.Array;
 public class Main {
     public static void main(String[] args) {
     String [] nombres = new String[]{"jorge","pedro","mati"};
-    for (int i = 0; i < nombres.length;i++){
-        System.out.println("Hola bienvenido " + nombres[i]);
+    for (int i = 0; i < nombres.length ;i++){
+        String nombresA = nombres[i] + " ";
+            for(int j = 0; j < 1; j++){
+                System.out.print(nombresA);
+        }
     }
     }
 
